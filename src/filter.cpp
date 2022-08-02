31d49a5acbb9cb36f44afb38b22b7920b3d6b5b2
taylor-bsg <taylor-bsg@users.noreply.github.com>
2022-08-02 16:32:45 -0700

    Update filter.cpp

--- src/filter.cpp
+++ src/filter.cpp
@@ -1,58 +1,80 @@
-/*
-* Copyright (c) 2014-2014 Wei Song <songw@cs.man.ac.uk>
-* Advanced Processor Technologies Group, School of Computer Science
-* University of Manchester, Manchester M13 9PL UK
-*
-* This source code is free software; you can redistribute it
-* and/or modify it in source code form under the terms of the GNU
-* General Public License as published by the Free Software
-* Foundation; either version 2 of the License, or (at your option)
-* any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
-*/
 
 /*
 * SAIF Filter Function
 *
-*
+* taylor-bsg
 */
+
+#include <iostream>
+#include <fstream>
 
 #include "saif_parser.hpp"
 #include "saif_db.hpp"
 #include <list>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 
 int main(int argc, char* argv[]) {
-  saif::SaifParser parser(argv[1]);
+  if (argc == 1) {
+    printf("Usage: filter <input .saif file> [ filter_list ] [ end filter_list ]*\n");
+    printf("filter_list is a sequence of hierarchical instance names within the path to keep signals for\n");
+    printf("note that ports, nets and instances at the higher levels will not be filtered\n");
+     exit(-1);
+  } 
+  
+   saif::SaifParser parser(argv[1]);
+   saif::SaifDB db;
+   
+   fprintf(stderr,"Start Parsing....\n");
+   parser.parse(&db);
+  
+   int fileNum = 0;
+   int curArg = 2;
+ 
+   while (curArg < argc)
+     {
+      pid_t pid;
+      int status=0;
 
-  std::list <std::string> filter_list;
+      std::list <std::string> filter_list;
 
-  for (int i = 2; i < argc; i++)
-    {
-      std::string filter = std::string(argv[i]);
-      filter_list.push_back(filter);
+      for (; curArg < argc; curArg++)
+       {
+         if (strcmp("end",argv[curArg]) == 0) 
+           { curArg++; break; }
+
+         std::string filter = std::string(argv[curArg]);
+         filter_list.push_back(filter);
+       }
+ 
+      std::cerr << "filter list:";
+      for (std::string x: filter_list) {
+       std::cerr << x << ' ';
+      }
+      std::cerr << "\n";
+
+      fprintf(stderr,"Start Filter....\n");
+
+      if (fork() == 0)
+       {
+         std::ofstream myfile;
+         char filename[500];
+         sprintf(filename,"%s_%d",argv[1],fileNum);
+         myfile.open(filename);
+
+         db.top->filter(filter_list);
+   
+         myfile << db << std::endl;
+         myfile.close();
+         std::cerr << "Generated " << filename << "\n";
+         exit(0);
+       }
+      else
+       wait(&status);
+     
+      fileNum++;
+     
+      return 0;
     }
-  /*
-  std::cout << "filter list:";
-  for (std::string x: filter_list) {
-    std::cout << x << ' ';
-  }
-  std::cout << "\n";
-  */
-  saif::SaifDB db;
-  
-  parser.parse(&db);
-
-  db.top->filter(filter_list);
-  
-  std::cout << db << std::endl;
-
-  return 0;
-}
